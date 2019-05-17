c703854481eee019ae749f3009c226958b0637f9
James Bowes <jbowes@repl.ca>
2019-05-17 12:51:42 -0300

    Fix synopsis for skip

--- skip/doc.go
+++ skip/doc.go
@@ -1,4 +1,6 @@
-// Package skip provides a frame skipping version of vice for re-use in APIs
-//
-// For complete  documentation, see the vice package.
+/*
+Package skip provides a frame skipping version of vice for re-use in APIs.
+
+For complete  documentation, see the vice package.
+*/
 package skip
